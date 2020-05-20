33d105b9db8164106cdb8a3d4357c894e9a09e2e
Julien Genestoux <julien.genestoux@gmail.com>
2020-05-20 15:50:41 -0400

    bringing cluster up first

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -3,7 +3,7 @@
 # First this script will deploy from an instance of unlock:latest
 REPO_ROOT=`dirname "$0"`/..
 BASE_DOCKER_COMPOSE=$REPO_ROOT/docker/docker-compose.yml
-DOCKER_COMPOSE_FILE=$REPO_ROOT/docker/docker-compose.ci.yml
+DOCKER_COMPOSE_CI=$REPO_ROOT/docker/docker-compose.ci.yml
 EXTRA_ARGS=$*
 
 mkdir -p /tmp/screenshots
@@ -11,7 +11,7 @@ chmod 0777 /tmp/screenshots
 
 # Take cluster down to start "clean"
 # TODO Let's make this optional via command line to make local dev easier
-docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE down
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_CI down
 
 # Improvment:
 # At this point, docker-compose will builds the images it needs for the cluster.
@@ -20,15 +20,18 @@ docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE down
 # improvments we want. We already have a script to build images with build-image.sh which should
 # make that easy.
 
+# Let's take the cluster up
+# This will give some time for servers to start (and run prestart scripts)
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_CI up
 
 # Deploy the subgraph
 # TODO make the script idempotent so that it does not deploy twice!
-docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE up subgraph_deployment
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_CI up subgraph_deployment
 
 # Deploy the smart contract
 # TODO: make the script idempotent so that it does not deploy twice!
-docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE up ganache-standup
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_CI up ganache-standup
 
 # And then run the integration tests
 COMMAND="yarn run ci"
-docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_FILE run -e UNLOCK_ENV=test -v /tmp/screenshots:/screenshots $EXTRA_ARGS integration-tests bash -c "$COMMAND"
+docker-compose -f $BASE_DOCKER_COMPOSE -f $DOCKER_COMPOSE_CI run -e UNLOCK_ENV=test -v /tmp/screenshots:/screenshots $EXTRA_ARGS integration-tests bash -c "$COMMAND"
